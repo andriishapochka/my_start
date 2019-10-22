381c95f74a7b8e78f85cdd46d2d0ff254e49a856
shapochka <andriishapochka@outlook.com>
2019-10-22 22:43:41 +0300

    1st commit

---
+++ My_firstApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace My_firstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
