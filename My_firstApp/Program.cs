71b5c45b1e5441ff3f8faff3c02ad1b9a81cc217
shapochka <andriishapochka@outlook.com>
2019-10-23 23:49:57 +0300

    переменные

--- My_firstApp/Program.cs
+++ My_firstApp/Program.cs
@@ -6,17 +6,39 @@ namespace My_firstApp
     {
         static void Main(string[] args)
         {
-            Calculator.Add(2, 3);
-            Console.ReadKey();
             
-        }
-    }
-    class Calculator
-    {
-        public static void Add(int x, int y)
-        {
-            int z = x + y;
-            Console.WriteLine($"Сума {x} та {y} дорiвнює {z}");
+            var apple = "Apple";
+
+            var index = 5; //визначає як int, якщо вже призначений тип - не можемо їй призначити інший тип
+
+            object obj2 = "Hello World";
+
+            object obj = 2;
+
+            string hello = "Hello";
+
+            char a = 'A';
+
+            decimal rate = 2.2M;
+
+            double weight = 111.25;
+
+            float hight = 234.17F;
+
+            byte b = 3;//0-255
+
+            bool isAlive = true;
+            isAlive = false; // 1 байт 
+
+            int tax = 9; //ініціалізація-присвоєння значення змінної при її визначенні 
+           
+            Console.WriteLine(tax);
+
+            Console.WriteLine(hello);
+
+            Console.WriteLine(obj2);
+
+            Console.ReadKey();
         }
     }
 }
