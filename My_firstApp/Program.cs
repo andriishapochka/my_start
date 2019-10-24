1822043a67e99d57806e3e9093da6a6fac79bd69
shapochka <andriishapochka@outlook.com>
2019-10-25 00:43:32 +0300

    присвоение переменной с плавающей запятой, для плучения числа без закругления к целому

--- My_firstApp/Program.cs
+++ My_firstApp/Program.cs
@@ -6,12 +6,10 @@ namespace My_firstApp
     {
         static void Main(string[] args)
         {
-            int x = 10;
-            int z = x / 4; // 2
-            int y = x * 5; // 50
+            double x = 10;
+            double z = x / 4; // 2, тк присвоили переменной с плавающей запятой 2.5
 
             Console.WriteLine(z);
-            Console.WriteLine(y);
 
             Console.ReadKey();
             
