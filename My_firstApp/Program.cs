3a03b7f33e88db400b94277a9bee40236d80973e
shapochka <andriishapochka@outlook.com>
2019-10-25 01:06:33 +0300

    инктрименто, дектрименто

--- My_firstApp/Program.cs
+++ My_firstApp/Program.cs
@@ -7,9 +7,23 @@ namespace My_firstApp
         static void Main(string[] args)
         {
             double x = 10;
-            double z = x / 4; // 2, тк присвоили переменной с плавающей запятой 2.5
+            double z = x % 4; // 2, остаток от деления целочисленного
+            int a = 4;
+            int b = ++a;
 
-            Console.WriteLine(z);
+            int c = 4;
+            int d = c++;
+
+            int e = 4;
+            int f = --e;
+
+            int j = 4;
+            int k = j--;
+                       
+            Console.WriteLine($"a = {a} b = {b} префиксный инкримент");
+            Console.WriteLine($"c {c} d = {d} постфиксный инкримент");
+            Console.WriteLine($"e = {e} f = {f} префиксный декримент");
+            Console.WriteLine($"j = {j} k = {k} постфиксный декримент");
 
             Console.ReadKey();
             
