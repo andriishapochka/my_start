9ead0655e7f1855186aca8e66f56f68b9bdf6aaa
shapochka <andriishapochka@outlook.com>
2019-10-26 01:19:22 +0300

    логические операции

--- My_firstApp/Program.cs
+++ My_firstApp/Program.cs
@@ -4,29 +4,50 @@ namespace My_firstApp
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) // логические операции
         {
-            double x = 10;
-            double z = x % 4; // 2, остаток от деления целочисленного
-            int a = 4;
-            int b = ++a;
+            // & | ^ ~
 
-            int c = 4;
-            int d = c++;
+           int x1 = 2; // 010
+           int y1 = 5; // 101
+                       // 000 - 0
 
-            int e = 4;
-            int f = --e;
+            Console.WriteLine(x1 & y1); // логическое умножение
 
-            int j = 4;
-            int k = j--;
-                       
-            Console.WriteLine($"a = {a} b = {b} префиксный инкримент");
-            Console.WriteLine($"c {c} d = {d} постфиксный инкримент");
-            Console.WriteLine($"e = {e} f = {f} префиксный декримент");
-            Console.WriteLine($"j = {j} k = {k} постфиксный декримент");
+            int x2 = 4; // 100
+            int y2 = 5; // 101
+                        // 100 - 4
 
+            Console.WriteLine(x2 & y2);
             Console.ReadKey();
-            
+
+
         }
+        //static void Main(string[] args) // инкрименто, декрименто, отстаток от деления целочисленного
+        //{
+
+
+        //    ////double x = 10;
+        //    //double z = x % 4; // 2, остаток от деления целочисленного
+        //    //int a = 4;
+        //    //int b = ++a;
+
+        //    //int c = 4;
+        //    //int d = c++;
+
+        //    //int e = 4;
+        //    //int f = --e;
+
+        //    //int j = 4;
+        //    //int k = j--;
+                       
+        //    //Console.WriteLine($"a = {a} b = {b} префиксный инкримент");
+        //    //Console.WriteLine($"c {c} d = {d} постфиксный инкримент");
+        //    //Console.WriteLine($"e = {e} f = {f} префиксный декримент");
+        //    //Console.WriteLine($"j = {j} k = {k} постфиксный декримент");
+
+        //    //Console.ReadKey();
+            
+        //}
     }
 }
