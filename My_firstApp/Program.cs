c0353576809f8e465989d0dc1da2ce3d9b0011d0
shapochka <andriishapochka@outlook.com>
2019-11-09 02:14:21 +0200

    enum #2

--- My_firstApp/Program.cs
+++ My_firstApp/Program.cs
@@ -4,26 +4,64 @@ namespace My_firstApp
 {
     class Program
     {//перечесление может быть в пределах класса програм или вне класа в пределах пространства имён 
-        enum Days// перечисление- логически связанные константы(можно указать тип перечисления- по умолчанию int и присваивается от 0 и віше на единицу)
-        {
-            Monday=1,//по умолчанию присвоится один, или присваиваем сами
-            Tuesday=11,
-            Wednesday=22,
-            Thursday=33,
-            Friday,// 34
-            Saturday,
-            Sunday//кроме после последнего не ставится запятая
-        }
+        
         static void Main(string[] args)
         {
-            Days day;
-            day = Days.Friday;
-            Console.WriteLine(day);
+            ApplyOperation(10, 15, Operation.Add);//25
+            ApplyOperation(10, 15, Operation.Multiply);//150
             Console.ReadKey();
+
+        }
+        enum Operation
+        {
+            Add,
+            Subtract,
+            Multiply,
+            Divide
+        }
+        static void ApplyOperation(double x, double y, Operation op)
+        {
+            double result = 0.0;
+            switch (op)
+            {
+                case Operation.Add:
+                    result = x + y;
+                    break;
+                case Operation.Subtract:
+                    result = x - y;
+                    break;
+                case Operation.Multiply:
+                    result = x * y;
+                    break;
+                case Operation.Divide:
+                    result = x / y;
+                    break;
+            }
+            Console.WriteLine(result);
 
         }
     }
 }
+//class Program
+//{//перечесление может быть в пределах класса програм или вне класа в пределах пространства имён 
+//    enum Days// перечисление- логически связанные константы(можно указать тип перечисления- по умолчанию int и присваивается от 0 и віше на единицу)
+//    {
+//        Monday = 1,//по умолчанию присвоится один, или присваиваем сами
+//        Tuesday = 11,
+//        Wednesday = 22,
+//        Thursday = 33,
+//        Friday,// 34
+//        Saturday,
+//        Sunday//кроме после последнего не ставится запятая
+//    }
+//    static void Main(string[] args)
+//    {
+//        Days day;
+//        day = Days.Friday;
+//        Console.WriteLine(day);
+//        Console.ReadKey();
+
+//    }
 //        static void Main(string[] args)
 //        {
 //            Addition(new int[] { 1, 2, 3, 4 }); //МОЖНО РАЗНЫМ СПОСОБОМ ВНОСИТЬ ПАРАМЕТРЫ
@@ -36,7 +74,7 @@ namespace My_firstApp
 //            for (int i = 0; i < numbers.Length; i++)
 //            {
 //                result += numbers[i];
-                
+
 //            }
 //            Console.WriteLine(result);
 //            Console.ReadKey();
@@ -97,179 +135,179 @@ namespace My_firstApp
 //        return x + y + z + s;
 //    }
 //}
-            //int perimeter;
-            //int area;
-            //GetData(10, 15, out area, out perimeter);
-            //Console.WriteLine($"Area = {area}");
-            //Console.WriteLine($"Perimeter = {perimeter}");
-            //Console.ReadKey();
+//int perimeter;
+//int area;
+//GetData(10, 15, out area, out perimeter);
+//Console.WriteLine($"Area = {area}");
+//Console.WriteLine($"Perimeter = {perimeter}");
+//Console.ReadKey();
 
 
 
-            //static void GetData(int width, int height, out int area, out int perimeter)
-            //{
-            //    perimeter = (width + height) * 2;
+//static void GetData(int width, int height, out int area, out int perimeter)
+//{
+//    perimeter = (width + height) * 2;
 
-            //    area = width * height;
-            //}
+//    area = width * height;
+//}
 
-            //int z = 5;
-            //int d = 6;
-            //// ввод параметров по значению ( используется копия значения и после выполнения метода не изменяется параметр вариант 2)
-            //Console.WriteLine($"Initial value z= {z}");
-            //AdditionVal(z, d);
-            //Console.WriteLine($"After AdditionVal z= {z}");
-            //AdditionVal(z, d);
-            //Console.WriteLine($"2After AdditionVal z= {z}");
-        ////    //// ввод параметрв по ссылке (используется само значение которое изменяется после выполнения метода вариант 2)
-        ////    //AdditionRef(ref z, d);
-        ////    //Console.WriteLine($"After AdditionRef z= {z}");
-        ////    //AdditionRef(ref z, d);
-        ////    //Console.WriteLine($"2After AdditionRef z= {z}");
+//int z = 5;
+//int d = 6;
+//// ввод параметров по значению ( используется копия значения и после выполнения метода не изменяется параметр вариант 2)
+//Console.WriteLine($"Initial value z= {z}");
+//AdditionVal(z, d);
+//Console.WriteLine($"After AdditionVal z= {z}");
+//AdditionVal(z, d);
+//Console.WriteLine($"2After AdditionVal z= {z}");
+////    //// ввод параметрв по ссылке (используется само значение которое изменяется после выполнения метода вариант 2)
+////    //AdditionRef(ref z, d);
+////    //Console.WriteLine($"After AdditionRef z= {z}");
+////    //AdditionRef(ref z, d);
+////    //Console.WriteLine($"2After AdditionRef z= {z}");
 
-        ////    //Console.ReadKey();
-        ////}
-        
-            //static void AdditionRef(ref int x, int y)
-            //{
-            //    x = x + y;
-            //    Console.WriteLine($"AdditionRef x={x}");
+////    //Console.ReadKey();
+////}
 
-            //}
+//static void AdditionRef(ref int x, int y)
+//{
+//    x = x + y;
+//    Console.WriteLine($"AdditionRef x={x}");
 
-            //static void AdditionVal(int x, int y)
-            //{
-            //    x= x + y;
-            //    Console.WriteLine($"AdditionVal x={x}");
-            //}
+//}
 
-            //static int Sum(int x, int y)
+//static void AdditionVal(int x, int y)
+//{
+//    x= x + y;
+//    Console.WriteLine($"AdditionVal x={x}");
+//}
 
-            //{
-            //   return x + y;
-            //}
-        
-    
+//static int Sum(int x, int y)
 
-        //{
-        //    int[] numbers = { 1, 3, 5, 7 };
-
-        //    for (int i = 0; i < 4; i++)// выведение значения массива с помощью for
-        //    {
-        //        Console.WriteLine(numbers[i]);
-
-        //    }
-        //    Console.ReadKey();
-        //}
-
-            //int[] numbers0 = { 1, 3, 5, 7 };// выведение значения массива с помощью foreach 
-            //foreach(int i in numbers0)
-            //{
-            //    Console.WriteLine(i);
-            //}
-            
-            //Console.ReadKey();
-
-        //    int[] numbers = new int[4] { 1, 2, 3, 4 }; // инициализация с ключ. new и указанием размера массива
-        //    int n = numbers[0]; // 1
-        //    numbers[0] = 7;
-        //    Console.WriteLine(numbers[3]);
-            
-
-        //    int[] numbers2 = { 2, 4, 6, 8 }; // без использования ключевого слова new
-        //    int t = numbers[0] = 5;
-        //    Console.WriteLine(numbers2[3]);
-
-        //    int[] numbers3 = new[] { 1, 3, 5, 7 };// иниц. с ключ словом new и без указания размера массива
-
-        //    Console.WriteLine(numbers3[2]);
-        //    Console.ReadKey();
-        //}
-
-        //static void Main(string[] args)
-        //{
-        //    int[] numbers = { 1, 2, 3, 4 }; // 0-3
-        //    int n = numbers[0]; // 1
-        //    numbers[0] = 7;
-        //    Console.WriteLine(numbers[0]);
-        //    Console.WriteLine(numbers[1]);
-        //    Console.WriteLine(numbers[2]);
-        //    Console.WriteLine(numbers[3]);
-        //    Console.ReadKey();
-
-        //}
-        //static void Main(string[] args) // факториал шахматнй доски
+//{
+//   return x + y;
+//}
 
 
-        //{
-        //int n = 64;
-        //double result = 1;
-        //for (int i = 1; i<=n; i++)
-        //  {
-        //        result = result * i;
-        //  }
-        //    Console.WriteLine(result);
-        //    Console.ReadKey();
-        //}
-           
-        //static void main(string[] args)
-    //{
-    //        byte a = 4;
-    //        int b = a + 70; // расширяющее преобразвание
-    //        // byte->short->int->long->decimal //компилятор преобразование производит сам
-    //        //byte->short->int->double  //компилятор преобразование производит сам
-    //        //byte->short->float->double //компилятор преобразование производит сам
-    //        //char->int   //компилятор преобразование производит сам
-    //        Console.WriteLine(b);
-    //        Console.ReadKey();
 
-    
-                //static void Main(string[] args) // логические операции
-       
-            // & | ^ ~
-            // XOR
+//{
+//    int[] numbers = { 1, 3, 5, 7 };
+
+//    for (int i = 0; i < 4; i++)// выведение значения массива с помощью for
+//    {
+//        Console.WriteLine(numbers[i]);
+
+//    }
+//    Console.ReadKey();
+//}
+
+//int[] numbers0 = { 1, 3, 5, 7 };// выведение значения массива с помощью foreach 
+//foreach(int i in numbers0)
+//{
+//    Console.WriteLine(i);
+//}
+
+//Console.ReadKey();
+
+//    int[] numbers = new int[4] { 1, 2, 3, 4 }; // инициализация с ключ. new и указанием размера массива
+//    int n = numbers[0]; // 1
+//    numbers[0] = 7;
+//    Console.WriteLine(numbers[3]);
 
 
-           //int x1 = 2; // 010
-           //int y1 = 5; // 101
-           //            // 000 - 0
+//    int[] numbers2 = { 2, 4, 6, 8 }; // без использования ключевого слова new
+//    int t = numbers[0] = 5;
+//    Console.WriteLine(numbers2[3]);
 
-           // Console.WriteLine(x1 & y1); // логическое умножение
+//    int[] numbers3 = new[] { 1, 3, 5, 7 };// иниц. с ключ словом new и без указания размера массива
 
-           // int x2 = 4; // 100
-           // int y2 = 5; // 101
-           //             // 100 - 4
+//    Console.WriteLine(numbers3[2]);
+//    Console.ReadKey();
+//}
 
-           // Console.WriteLine(x2 & y2);
-           // Console.ReadKey();
+//static void Main(string[] args)
+//{
+//    int[] numbers = { 1, 2, 3, 4 }; // 0-3
+//    int n = numbers[0]; // 1
+//    numbers[0] = 7;
+//    Console.WriteLine(numbers[0]);
+//    Console.WriteLine(numbers[1]);
+//    Console.WriteLine(numbers[2]);
+//    Console.WriteLine(numbers[3]);
+//    Console.ReadKey();
+
+//}
+//static void Main(string[] args) // факториал шахматнй доски
 
 
-     
-        //static void Main(string[] args) // инкрименто, декрименто, отстаток от деления целочисленного
-        //{
+//{
+//int n = 64;
+//double result = 1;
+//for (int i = 1; i<=n; i++)
+//  {
+//        result = result * i;
+//  }
+//    Console.WriteLine(result);
+//    Console.ReadKey();
+//}
+
+//static void main(string[] args)
+//{
+//        byte a = 4;
+//        int b = a + 70; // расширяющее преобразвание
+//        // byte->short->int->long->decimal //компилятор преобразование производит сам
+//        //byte->short->int->double  //компилятор преобразование производит сам
+//        //byte->short->float->double //компилятор преобразование производит сам
+//        //char->int   //компилятор преобразование производит сам
+//        Console.WriteLine(b);
+//        Console.ReadKey();
 
 
-        //    ////double x = 10;
-        //    //double z = x % 4; // 2, остаток от деления целочисленного
-        //    //int a = 4;
-        //    //int b = ++a;
+//static void Main(string[] args) // логические операции
 
-        //    //int c = 4;
-        //    //int d = c++;
+// & | ^ ~
+// XOR
 
-        //    //int e = 4;
-        //    //int f = --e;
 
-        //    //int j = 4;
-        //    //int k = j--;
-                       
-        //    //Console.WriteLine($"a = {a} b = {b} префиксный инкримент");
-        //    //Console.WriteLine($"c {c} d = {d} постфиксный инкримент");
-        //    //Console.WriteLine($"e = {e} f = {f} префиксный декримент");
-        //    //Console.WriteLine($"j = {j} k = {k} постфиксный декримент");
+//int x1 = 2; // 010
+//int y1 = 5; // 101
+//            // 000 - 0
 
-        //    //Console.ReadKey();
-            
-        //}
-    
+// Console.WriteLine(x1 & y1); // логическое умножение
+
+// int x2 = 4; // 100
+// int y2 = 5; // 101
+//             // 100 - 4
+
+// Console.WriteLine(x2 & y2);
+// Console.ReadKey();
+
+
+
+//static void Main(string[] args) // инкрименто, декрименто, отстаток от деления целочисленного
+//{
+
+
+//    ////double x = 10;
+//    //double z = x % 4; // 2, остаток от деления целочисленного
+//    //int a = 4;
+//    //int b = ++a;
+
+//    //int c = 4;
+//    //int d = c++;
+
+//    //int e = 4;
+//    //int f = --e;
+
+//    //int j = 4;
+//    //int k = j--;
+
+//    //Console.WriteLine($"a = {a} b = {b} префиксный инкримент");
+//    //Console.WriteLine($"c {c} d = {d} постфиксный инкримент");
+//    //Console.WriteLine($"e = {e} f = {f} префиксный декримент");
+//    //Console.WriteLine($"j = {j} k = {k} постфиксный декримент");
+
+//    //Console.ReadKey();
+
+//}
+
 
